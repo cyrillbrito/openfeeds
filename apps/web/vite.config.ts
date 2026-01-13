418f174e796066aeb0aa635d07ab282a289e635b
Cyrill brito <cyrill.brito@gmail.com>
2026-01-13 19:59:41 +0000

    Stabilize versions while solving build issues

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,13 +3,15 @@ import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
+import lucidePreprocess from 'vite-plugin-lucide-preprocess';
 import solidPlugin from 'vite-plugin-solid';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    lucidePreprocess(),
     devtools(),
-    nitro({ preset: 'bun', prerender: { routes: [] } }),
+    nitro(),
     viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart({ spa: { enabled: true } }),
