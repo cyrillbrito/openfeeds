38dfa1f5e3b57831d76b6ce0965d77d828310541
Cyrill brito <cyrill.brito@gmail.com>
2026-01-16 20:06:05 +0000

    Fix docker build, by ignoring the error, might be some issue with tanstack start

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -14,7 +14,11 @@ export default defineConfig({
     nitro(),
     viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackStart({ spa: { enabled: true } }),
+    tanstackStart({
+      spa: { enabled: true },
+      // For some crazy reason, it still tries to prerender
+      prerender: { enabled: false, failOnError: false },
+    }),
     solidPlugin({ ssr: true }),
   ],
 });
