618ce538ea0266329a96bf2af37222ee36931ddf
Cyrill Brito <cyrill.brito@gmail.com>
2026-02-16 18:41:32 +0000

    chore: remove unused files

--- apps/web/src/components/HeaderLink.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSXElement } from 'solid-js';
-
-interface HeaderLinkProps {
-  children: JSXElement;
-}
-
-export function HeaderLinkStyle(props: HeaderLinkProps) {
-  return <span class="link link-hover font-semibold">{props.children}</span>;
-}
